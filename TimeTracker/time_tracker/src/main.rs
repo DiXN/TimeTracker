74149c810d088f18371ac7494e10a8b4c407c4eb
Michael Kaltschmid <kaltschmidmichael@gmail.com>
2019-05-16 03:15:49 +0200

    Make Firebase and Postgres features.

--- TimeTracker/time_tracker/src/main.rs
+++ TimeTracker/time_tracker/src/main.rs
@@ -1,29 +1,28 @@
 #[macro_use] extern crate log;
 #[macro_use] extern crate lazy_static;
 
-use std::{
-  fs,
-  error::Error
-};
+use std::error::Error;
 
 use libc::c_char;
 
 use env_logger::{Builder, Env};
-use serde_json::Value;
 
 mod receive_types;
 mod time_tracking;
 
+#[cfg(feature = "firebase")]
 mod firebase;
+#[cfg(feature = "firebase")]
 use crate::firebase::FirebaseClient;
 
 mod restable;
 
 mod rpc;
 
+#[cfg(feature = "psql")]
 mod sql;
+#[cfg(feature = "psql")]
 use crate::sql::PgClient;
-use crate::restable::Restable;
 
 extern {
   pub fn query_file_info(process: *const c_char) -> *const c_char;
@@ -51,20 +50,30 @@ macro_rules! ns_invoke {
   }};
 }
 
+#[cfg(feature = "firebase")]
+fn init_client() {
+  use std::fs;
+  use serde_json::Value;
+
+  let config = fs::read_to_string("env.json")?;
+  let config : Value = serde_json::from_str(&config)?;
+  let firebase_client = FirebaseClient::new(config["url"].as_str().unwrap(), config["key"].as_str().unwrap());
+  time_tracking::init(firebase_client).unwrap();
+}
+
+#[cfg(feature = "psql")]
+fn init_client() {
+  let pg_client = PgClient::new("postgres://postgres:root@10.0.0.5:5432/time_tracker");
+  time_tracking::init(pg_client).unwrap();
+}
+
+
 fn main() -> Result<(), Box<dyn Error>> {
   let env = Env::default()
     .filter_or(env_logger::DEFAULT_FILTER_ENV, "info");
 
   Builder::from_env(env).init();
 
-  let config = fs::read_to_string("env.json")?;
-  let config : Value = serde_json::from_str(&config)?;
-  //let firebase_client = FirebaseClient::new(config["url"].as_str().unwrap(), config["key"].as_str().unwrap());
-  //time_tracking::init(firebase_client).unwrap();
-
-  let pg_client = PgClient::new("postgres://postgres:root@10.0.0.5:5432/time_tracker");
-  println!("{:?}", pg_client.get_processes());
-
-
+  init_client();
   Ok(())
 }
