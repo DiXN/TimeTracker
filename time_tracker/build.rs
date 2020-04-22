9993d53754c70f3e8e5a1f1ecd452e1fed5663a5
Michael Kaltschmid <kaltschmidmichael@gmail.com>
2020-04-22 02:21:18 +0200

    Use `embeded-resource` instead of `windres`.

--- time_tracker/build.rs
+++ time_tracker/build.rs
@@ -1,9 +1,6 @@
 #[cfg(target_os = "windows")]
-use windres::Build;
-
-#[cfg(target_os = "windows")]
 fn main() {
-  Build::new().compile("src/windows/time_tracker.rc").unwrap();
+  embed_resource::compile("src/windows/time_tracker.rc");
 }
 
 #[cfg(not(target_os = "windows"))]
